7c798d5963b3d439a4940bdc304a783432e719f3
Andrea Nasuto <andrea.nasuto@gmail.com>
2018-02-13 23:54:45 +0000

    automatically backed up by learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -7,7 +7,7 @@ module ArtistsHelper
     if artist.nil?
       select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
     else
-      hidden_field_tag ""
+      hidden_field_tag "artist_id"
     end
   end
 end
